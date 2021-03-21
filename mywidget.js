0acf7c4317d3dd0ff201f5dc8702fe1e3657e9e1
mace <mace_blue@hotmail.com>
2021-03-21 22:21:46 +0100

    uncomment soap function

--- mywidget.js
+++ mywidget.js
@@ -1,8 +1,7 @@
-
-let widget = createWidget()
 let text = await textFromSoap()
 
 if (!config.runsInWidget) {
+	let widget = createWidget(text)
 	widget.presentSmall();
 }
 Script.setWidget(widget)
@@ -22,34 +21,33 @@ function createWidget(text) {
 }
 
 async function textFromSoap() {
-	return 'text aus soap';
-    // var xmlhttp = new XMLHttpRequest();
-    // xmlhttp.open('POST', 'http://192.168.178.1:49000/upnp/control/hosts', true);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'http://192.168.178.1:49000/upnp/control/hosts', true);
 
-    // // build SOAP request
-    // var sr =
-    //     '<?xml version="1.0" encoding="utf-8"?>' +
-    //     '<soapenv:Envelope ' + 
-    //         'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-    //         'xmlns:api="urn:dslforum-org:service:Hosts:1" ' +
-    //         'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-    //         'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
-    //         '<soapenv:Body>' +
-    //             '<api:GetHostNumberOfEntries></api:GetHostNumberOfEntries>' +
-    //         '</soapenv:Body>' +
-    //     '</soapenv:Envelope>';
+    // build SOAP request
+    var sr =
+        '<?xml version="1.0" encoding="utf-8"?>' +
+        '<soapenv:Envelope ' + 
+            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+            'xmlns:api="urn:dslforum-org:service:Hosts:1" ' +
+            'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+            'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
+            '<soapenv:Body>' +
+                '<api:GetHostNumberOfEntries></api:GetHostNumberOfEntries>' +
+            '</soapenv:Body>' +
+        '</soapenv:Envelope>';
 
-    // xmlhttp.onreadystatechange = function () {
-    //     if (xmlhttp.readyState == 4) {
-    //         if (xmlhttp.status == 200) {
-    //             return xmlhttp.responseText;
-    //             // alert('done. use firebug/console to see network response');
-    //         } else {
-    //             return 'error';
-    //         }
-    //     }
-    // }
-    // // Send the POST request
-    // xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    // xmlhttp.send(sr);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                return xmlhttp.responseText;
+                // alert('done. use firebug/console to see network response');
+            } else {
+                return 'error';
+            }
+        }
+    }
+    // Send the POST request
+    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+    xmlhttp.send(sr);
 }
