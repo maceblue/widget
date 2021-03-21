0739d8341a1fa6504163eb7591470273b83d58b7
mace <mace_blue@hotmail.com>
2021-03-21 22:15:08 +0100

    comment soap function

--- mywidget.js
+++ mywidget.js
@@ -1,61 +1,55 @@
-class MyWidget
-{
-	run()
-	{
-		let widget = createWidget()
-		if (!config.runsInWidget) {
-			widget.presentSmall();
-		}
-		Script.setWidget(widget)
-		Script.complete()
 
-		function createWidget() {
-			let w = new ListWidget()
-		  	w.backgroundColor = new Color("#4a6da7")
+let widget = createWidget()
+let text = await textFromSoap()
 
-		  	let response = await soap();
+if (!config.runsInWidget) {
+	widget.presentSmall();
+}
+Script.setWidget(widget)
+Script.complete()
 
-		  	let article = w.addText(response)
-		  	article.textColor = Color.white()
-		  	article.textOpacity = 0.8
-		  	article.textSize = 12
+function createWidget(text) {
+	let w = new ListWidget()
+  	w.backgroundColor = new Color("#4a6da7")
 
-		  return w
-		}
 
-		function soap() {
-			return 'text aus soap';
-            // var xmlhttp = new XMLHttpRequest();
-            // xmlhttp.open('POST', 'http://192.168.178.1:49000/upnp/control/hosts', true);
+  	let article = w.addText(text)
+  	article.textColor = Color.white()
+  	article.textOpacity = 0.8
+  	article.textSize = 12
 
-            // // build SOAP request
-            // var sr =
-            //     '<?xml version="1.0" encoding="utf-8"?>' +
-            //     '<soapenv:Envelope ' + 
-            //         'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-            //         'xmlns:api="urn:dslforum-org:service:Hosts:1" ' +
-            //         'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-            //         'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
-            //         '<soapenv:Body>' +
-            //             '<api:GetHostNumberOfEntries></api:GetHostNumberOfEntries>' +
-            //         '</soapenv:Body>' +
-            //     '</soapenv:Envelope>';
-
-            // xmlhttp.onreadystatechange = function () {
-            //     if (xmlhttp.readyState == 4) {
-            //         if (xmlhttp.status == 200) {
-            //             return xmlhttp.responseText;
-            //             // alert('done. use firebug/console to see network response');
-            //         } else {
-            //             return 'error';
-            //         }
-            //     }
-            // }
-            // // Send the POST request
-            // xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-            // xmlhttp.send(sr);
-        }
-	}
+  return w
 }
 
-new MyWidget().run();
+async function textFromSoap() {
+	return 'text aus soap';
+    // var xmlhttp = new XMLHttpRequest();
+    // xmlhttp.open('POST', 'http://192.168.178.1:49000/upnp/control/hosts', true);
+
+    // // build SOAP request
+    // var sr =
+    //     '<?xml version="1.0" encoding="utf-8"?>' +
+    //     '<soapenv:Envelope ' + 
+    //         'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+    //         'xmlns:api="urn:dslforum-org:service:Hosts:1" ' +
+    //         'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+    //         'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
+    //         '<soapenv:Body>' +
+    //             '<api:GetHostNumberOfEntries></api:GetHostNumberOfEntries>' +
+    //         '</soapenv:Body>' +
+    //     '</soapenv:Envelope>';
+
+    // xmlhttp.onreadystatechange = function () {
+    //     if (xmlhttp.readyState == 4) {
+    //         if (xmlhttp.status == 200) {
+    //             return xmlhttp.responseText;
+    //             // alert('done. use firebug/console to see network response');
+    //         } else {
+    //             return 'error';
+    //         }
+    //     }
+    // }
+    // // Send the POST request
+    // xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+    // xmlhttp.send(sr);
+}
