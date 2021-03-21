4ce51f376b25761a6b0718598024b54be37c0fee
mace <mace_blue@hotmail.com>
2021-03-21 20:27:23 +0100

    add soap function

--- mywidget.js
+++ mywidget.js
@@ -4,7 +4,7 @@ class MyWidget
 	{
 		let widget = createWidget()
 		if (!config.runsInWidget) {
-			widget.presentsSmall();
+			widget.presentSmall();
 		}
 		Script.setWidget(widget)
 		Script.complete()
@@ -13,13 +13,47 @@ class MyWidget
 			let w = new ListWidget()
 		  	w.backgroundColor = new Color("#4a6da7")
 
-		  	let article = w.addText('Hello World')
+		  	let response = await soap();
+
+		  	let article = w.addText(response)
 		  	article.textColor = Color.white()
 		  	article.textOpacity = 0.8
 		  	article.textSize = 12
 
 		  return w
 		}
+
+		function soap() {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open('POST', 'http://192.168.178.1:49000/upnp/control/hosts', true);
+
+            // build SOAP request
+            var sr =
+                '<?xml version="1.0" encoding="utf-8"?>' +
+                '<soapenv:Envelope ' + 
+                    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+                    'xmlns:api="urn:dslforum-org:service:Hosts:1" ' +
+                    'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+                    'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
+                    '<soapenv:Body>' +
+                        '<api:GetHostNumberOfEntries></api:GetHostNumberOfEntries>' +
+                    '</soapenv:Body>' +
+                '</soapenv:Envelope>';
+
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4) {
+                    if (xmlhttp.status == 200) {
+                        return xmlhttp.responseText;
+                        // alert('done. use firebug/console to see network response');
+                    } else {
+                        return 'error';
+                    }
+                }
+            }
+            // Send the POST request
+            xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+            xmlhttp.send(sr);
+        }
 	}
 }
 
